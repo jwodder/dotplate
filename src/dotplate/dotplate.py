349185d08c1a7ff4d1019a9233d0f6f7d381beeb
John T. Wodder II <git@varonathe.org>
2025-01-08 22:07:40 -0500

    Progress

--- src/dotplate/dotplate.py
+++ src/dotplate/dotplate.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from difflib import unified_diff
 from enum import Enum
 from pathlib import Path
 from typing import Any
 from .config import Config
+from .util import is_executable, listdir
 
 
 @dataclass
@@ -17,7 +19,7 @@ class Dotplate:
         raise NotImplementedError
 
     def src_paths(self) -> list[str]:
-        raise NotImplementedError
+        return listdir(self.cfg.dest)
 
     def render(self, src_path: str) -> RenderedFile:
         raise NotImplementedError
@@ -38,7 +40,35 @@ class RenderedFile:
     executable: bool = False
 
     def diff(self, dest_path: Path | None) -> Diff:
-        raise NotImplementedError
+        if dest_path is None:
+            dest_path = self.dest_path
+        try:
+            with dest_path.open("r", encoding="utf-8") as fp:
+                dest_content = fp.read()
+        except FileNotFoundError:
+            dest_content = ""
+            state = DiffState.MISSING
+            xbit_diff = XBitDiff.REMOVED if self.executable else XBitDiff.NOCHANGE
+        else:
+            state = (
+                DiffState.NODIFF if dest_content == self.content else DiffState.CHANGED
+            )
+            match (self.executable, is_executable(dest_path)):
+                case (True, False):
+                    xbit_diff = XBitDiff.REMOVED
+                case (False, True):
+                    xbit_diff = XBitDiff.ADDED
+                case _:
+                    xbit_diff = XBitDiff.NOCHANGE
+        delta = "".join(
+            unified_diff(
+                dest_content.splitlines(True),
+                self.content.splitlines(True),
+                fromfile=self.src_path,
+                tofile=str(dest_path),
+            )
+        )
+        return Diff(delta=delta, state=state, xbit_diff=xbit_diff)
 
     def install(self, dest_path: Path | None = None) -> None:
         raise NotImplementedError
@@ -49,7 +79,7 @@ class RenderedFile:
 
 @dataclass
 class Diff:
-    text: str
+    delta: str
     state: DiffState
     xbit_diff: XBitDiff
 
---
+++ src/dotplate/util.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+import os
+from pathlib import Path
+import stat
+import subprocess
+from iterpath import iterpath
+from linesep import split_terminated
+
+
+def listdir(dirpath: Path) -> list[str]:
+    """
+    List the files in `dirpath`, relative to `dirpath` and
+    forward-slash-separated.  If `dirpath` is in a Git repository, only files
+    known to Git are returned.
+    """
+    if in_git(dirpath):
+        # TODO: Try to include files staged but not yet committed
+        # (Merge a variant of `git diff --cached --name-status`?)
+        r = subprocess.run(
+            ["git", "ls-tree", "-r", "--name-only", "-z"],
+            cwd=dirpath,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+        )
+        return split_terminated(r.stdout, "\0")
+    else:
+        with iterpath(dirpath, dirs=False, return_relative=True, sort=True) as ip:
+            return [str(p) for p in ip]
+
+
+def in_git(dirpath: Path) -> bool:
+    """Test whether `dirpath` is under Git revision control"""
+    try:
+        r = subprocess.run(
+            ["git", "rev-parse", "--is-inside-work-tree"],
+            cwd=dirpath,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+        )
+        if r.stdout.strip() == "false":
+            # We are inside a .git directory
+            return False
+    except FileNotFoundError:
+        # Git not installed; assume this isn't a Git repository
+        return False
+    except subprocess.CalledProcessError:
+        return False
+    # Check whether `path` is tracked by Git (Note that we can't rely
+    # on this check alone, as it succeeds when inside a .git/ directory)
+    return (
+        subprocess.run(
+            ["git", "ls-files", "--error-unmatch", "."],
+            cwd=dirpath,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        ).returncode
+        == 0
+    )
+
+
+def is_executable(p: Path) -> bool:
+    if os.name == "posix":
+        return p.stat().st_mode & stat.S_IXUSR != 0
+    else:
+        return False
